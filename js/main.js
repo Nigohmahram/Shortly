97102b04dc4132aa18b869b2cd4ea3c730869c62
Nigohmahram <usmonovmaxsudovich@gmail.com>
2022-11-22 23:07:52 +0900

    Add url copy button click

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const elUrlShortener = document.querySelector('.url-shortener');
+const elUrlShortenerForm = document.querySelector('.js-url-shortener-form');
+const elUrlShortenerResults = document.querySelector('.url-shortener__results');
+const elCopyUrlShortLinkButton = document.querySelector('.js-copy-short-link-button');
+
+if (elUrlShortenerForm) {
+     elUrlShortenerForm.addEventListener('submit', function (evt) {
+          evt.preventDefault();
+
+          elUrlShortenerResults.classList.add('url-shortener__results--open');
+     });
+}
+
+
+if (elUrlShortener) {
+     elUrlShortener.addEventListener('click', function (evt) {
+          if (evt.target.matches('.js-copy-short-link-button')) {
+               //Change button text
+               evt.target.textContent = 'Copied!';
+               //change button bgcol
+               evt.target.classList.add('url-shortener__copy-button--copied');
+
+               // Reset button text and class after 1 secund
+               setTimeout(function () {
+                    evt.target.textContent = 'Copy';
+                    evt.target.classList.remove('url-shortener__copy-button--copied');
+               }, 1000);
+          }
+     });
+}
